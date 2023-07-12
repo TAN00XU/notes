f63d6f62061c7fecb9128b1729ab261f13533e31
饮梦 <tan00xu@163.com>
2023-07-12 13:41:55 +0800

    提交笔记.bat

---
+++ 提交笔记.bat
@@ -0,0 +1,11 @@
+%开关回响on/off%
+echo on
+title bat批处理执行git命令
+E:
+cd D:\TX\Desktop\notes
+git add .
+git commit -m %date:~0,4%年%date:~5,2%月%date:~8,2%日%time:~0,2%时%time:~3,2%分%time:~6,2%秒%time:~9,2%
+git push gitee master
+git push github master
+%暂停命令防止cmd直接关闭，便于查看执行情况%
+pause
