c320d608433353b431011e7ce4ee0203c892fafd
RealDream <tan00xu@163.com>
2024-04-18 10:32:58 +0800

    2024年04月18日10时32分58秒86

--- 提交笔记.bat
+++ 提交笔记.bat
@@ -2,7 +2,7 @@
 echo on
 title bat批处理执行git命令
 E:
-cd D:\TX\Desktop\notes
+cd D:\RealDream\Documents\Code\notes
 git add .
 git commit -m %date:~0,4%年%date:~5,2%月%date:~8,2%日%time:~0,2%时%time:~3,2%分%time:~6,2%秒%time:~9,2%
 git push gitee master
